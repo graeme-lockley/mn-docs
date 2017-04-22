74d4eeb9607c1a449435917231b9a9eb7f55883a
Graeme Lockley <graemel@no9.co.za>
2017-04-22 15:40:33 +0200

    feature: Parse JavaScript Input Suite: given a header, an mrequire import and functions with a signature but without any assumptions

--- Source.js
+++ Source.js
@@ -1,5 +1,6 @@
 const Array = mrequire("core:Data.Array:v1.0.0");
 const FileSystem = require("./System/IO/Native/FileSystem");
+const Maybe = mrequire("core:Data.Maybe:v1.0.0");
 const NativeArray = mrequire("core:Data.Native.Array:1.0.0");
 const NativeString = require("./Data/Native/String");
 
@@ -23,11 +24,12 @@ function parseSource(source) {
     let functions = Array.empty;
 
     let currentFunctionHeader = Array.empty;
+    let currentFunctionSignature = Maybe.Nothing;
     let currentFunctionAssumptions = Array.empty;
 
     for (let index = 0; index < input.length; index += 1) {
         const line = input[index];
-        switch(state) {
+        switch (state) {
             case 0:
                 if (NativeString.startsWith("//-")(line)) {
                     header = header.append(removeComment(line));
@@ -55,9 +57,47 @@ function parseSource(source) {
                     if (match.isJust()) {
                         imports = imports.append(match.reduce(() => "")(a => NativeArray.at(a)(1).reduce(() => "")(i => i)));
                     }
+                    break;
                 }
-
+            case 3:
+                if (NativeString.startsWith("//-")(line)) {
+                    currentFunctionHeader = currentFunctionHeader.append(removeComment(line));
+                    state = 3;
+                } else if (NativeString.startsWith("//=")(line)) {
+                    currentFunctionSignature = Maybe.Just(removeComment(line));
+                    state = 4;
+                } else {
+                    state = 4;
+                }
+                break;
+            case 4:
+                if (NativeString.startsWith("//-")(line) || NativeString.startsWith("//=")(line)) {
+                    if (currentFunctionSignature.isJust()) {
+                        functions = functions.append({
+                            header: currentFunctionHeader,
+                            signature: currentFunctionSignature,
+                            assumptions: currentFunctionAssumptions
+                        });
+                        currentFunctionHeader = Array.empty;
+                        currentFunctionSignature = Maybe.Nothing;
+                        currentFunctionAssumptions = Array.empty;
+                    }
+                    if (NativeString.startsWith("//-")(line)) {
+                        currentFunctionHeader = currentFunctionHeader.append(removeComment(line));
+                        state = 3;
+                    } else if (NativeString.startsWith("//=")(line)) {
+                        currentFunctionSignature = Maybe.Just(removeComment(line));
+                        state = 4;
+                    }
+                }
         }
+    }
+    if (currentFunctionSignature.isJust()) {
+        functions = functions.append({
+            header: currentFunctionHeader,
+            signature: currentFunctionSignature,
+            assumptions: currentFunctionAssumptions
+        });
     }
 
     return {
