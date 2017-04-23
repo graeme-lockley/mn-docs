b8cb16b706906f2b6442360e797a6aef34d3ac39
Graeme Lockley <graemel@no9.co.za>
2017-04-23 08:41:08 +0200

    refactor: change implementation to a Promise based implementation use System.IO.Native.FileSystem rather than fs

--- Test/ParseInputTest.js
+++ Test/ParseInputTest.js
@@ -1,5 +1,5 @@
 const Assert = require("assert");
-const FS = require("fs");
+const FileSystem = require("../System/IO/Native/FileSystem");
 const Unit = mrequire("core:Test.Unit:v1.0.0");
 
 const Source = require("../Source");
@@ -17,47 +17,52 @@ forAllScenariosIn(__dirname + "/ParseInputScenarios", (name, input, expectations
             Assert.deepEqual(output, expectations["doc"]);
         });
     }
-})
-;
+});
 
 
 function forAllScenariosIn(location, assertions) {
-    FS.readdirSync(location).forEach(f => {
-        const contents = FS.readFileSync(location + '/' + f).toString().split('\n');
+    FileSystem.readdir(location)
+        .then(files => files.map(f => {
+            FileSystem.readFile(location + '/' + f)
+                .then(f => f.split('\n'))
+                .then(contents => {
+                    let name = '';
+                    let input = [];
+                    let currentExpectation;
+                    let expectations = {};
+                    let output = [];
 
-        let name = '';
-        let input = [];
-        let currentExpectation;
-        let expectations = {};
-        let output = [];
+                    let state = 0;
 
-        let state = 0;
+                    for (let index = 0; index < contents.length; index += 1) {
+                        if (state === 0) {
+                            name = contents[index].substring(2).trim();
+                            state = 1;
+                        } else if (state === 1) {
+                            if (contents[index].startsWith('--')) {
+                                currentExpectation = contents[index].substring(2).trim();
+                                output = [];
+                                state = 3;
+                            } else {
+                                input.push(contents[index]);
+                            }
+                        } else {
+                            if (contents[index].startsWith('--')) {
+                                expectations[currentExpectation] = output.join('\n');
+                                currentExpectation = contents[index].substring(2).trim();
+                                output = [];
+                            } else {
+                                output.push(contents[index]);
+                            }
+                        }
+                    }
 
-        for (let index = 0; index < contents.length; index += 1) {
-            if (state === 0) {
-                name = contents[index].substring(2).trim();
-                state = 1;
-            } else if (state === 1) {
-                if (contents[index].startsWith('--')) {
-                    currentExpectation = contents[index].substring(2).trim();
-                    output = [];
-                    state = 3;
-                } else {
-                    input.push(contents[index]);
-                }
-            } else {
-                if (contents[index].startsWith('--')) {
                     expectations[currentExpectation] = output.join('\n');
-                    currentExpectation = contents[index].substring(2).trim();
-                    output = [];
-                } else {
-                    output.push(contents[index]);
-                }
-            }
-        }
 
-        expectations[currentExpectation] = output.join('\n');
+                    assertions(f + ": " + name, input.join('\n'), expectations);
+                })
+                .catch(err => console.error(`Failed: ${f}: ${err}`));
+        }))
+        .catch(console.error);
 
-        assertions(f + ": " + name, input.join('\n'), expectations);
-    });
 }
